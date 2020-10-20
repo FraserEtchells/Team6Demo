82efaa953287bd8a83eb6e5b4fbdb0392c152983
Fraser Etchells <fraseretchells1@hotmail.co.uk>
2020-10-20 15:53:28 +0100

    Update Quiz.jsx

--- src/Components/Quiz.jsx
+++ src/Components/Quiz.jsx
@@ -6,7 +6,7 @@ import { createGlobalStyle } from 'styled-components'
 import openSocket from "socket.io-client";
 import PassionOne from '../fonts/PassionOne.ttf';
 
-const socket = openSocket('https://demoteam6.azurewebsites.net:443');
+const socket = openSocket('https://demoteam6.azurewebsites.net/socket.io/?EIO=4&transport=websocket');
 
 const GlobalStyle = createGlobalStyle`
  @font-face {
