08960107ca9917bf530c8f4c35b3659718b8f0ac
Fraser Etchells <fraseretchells1@hotmail.co.uk>
2020-10-20 16:11:11 +0100

    edited web.config

--- src/Components/Quiz.jsx
+++ src/Components/Quiz.jsx
@@ -6,7 +6,7 @@ import { createGlobalStyle } from 'styled-components'
 import openSocket from "socket.io-client";
 import PassionOne from '../fonts/PassionOne.ttf';
 
-const socket = openSocket('https://demoteam6.azurewebsites.net/socket.io/?EIO=4&transport=websocket');
+const socket = openSocket('https://demoteam6.azurewebsites.net/?EIO=3&transport=polling&t=NL6W9oS');
 
 const GlobalStyle = createGlobalStyle`
  @font-face {
